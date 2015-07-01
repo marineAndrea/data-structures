25d0fd7548b3f0d3660ef975e21f1afed82a2cf3
marineAndrea <marine.guillanton@gmail.com>
2015-07-01 16:32:55 -0700

    "stack and queue done"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var queueSize = 0;
+  var dequeuedTimes = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[queueSize] = value; // 0:a 1:b
+    queueSize++; // 2
   };
 
   someInstance.dequeue = function(){
+    dequeuedTimes++;
+    return storage[dequeuedTimes - 1];
   };
 
-  someInstance.size = function(){
+  someInstance.size = function() {
+    if (queueSize - dequeuedTimes <= 0) {
+      return 0;
+    }
+    return queueSize - dequeuedTimes;
   };
 
   return someInstance;

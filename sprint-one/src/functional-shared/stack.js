6a1627f54d979723beb0bb0a2f4b45fd9ccd9be6
marineAndrea <marine.guillanton@gmail.com>
2015-07-01 20:02:56 -0700

    "stackMethods push in progress"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  var length = 0;
+  var obj = {};
+  extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push : function(value) {
+		storage[length] = value;
+		length++;
+		
+	},
+	pop : function() {
+		return;
 
+	},
+	size : function() {
+		return length;
+	}
+};
 
+var extend = function(constr, methods) {
+	for (var key in methods) {
+		constr[key] = methods[key];
+	}
+	return constr;
+};

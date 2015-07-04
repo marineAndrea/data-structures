d0ad82f3d4f815110692cd7ba874450789c34af1
marineAndrea <marine.guillanton@gmail.com>
2015-07-03 21:52:44 -0700

    "Graph and Set completed"

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -16,6 +16,7 @@ Graph.prototype.contains = function(node){
 Graph.prototype.removeNode = function(node){
   var index = _.indexOf(this.nodes, node);
   this.nodes.splice(index, 1);
+  this.connections.splice(index, 1);
 
   // this.connections.slice(index, 1);
 };
@@ -32,15 +33,26 @@ Graph.prototype.hasEdge = function(fromNode, toNode){
 
 Graph.prototype.addEdge = function(fromNode, toNode){
   var indexFrom = _.indexOf(this.nodes, fromNode);
+  var indexTo = _.indexOf(this.nodes, toNode);
   this.connections[indexFrom].push(toNode);
+  this.connections[indexTo].push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var indexFrom = _.indexOf(this.nodes, fromNode);
+  var indexTo = _.indexOf(this.nodes, toNode);
+  var removeIndex1 = _.indexOf(this.connections[indexFrom], this.nodes[indexTo]);
+  this.connections[indexFrom].splice(removeIndex1, 1);
+  var removeIndex2 = _.indexOf(this.connections[indexTo], this.nodes[indexFrom]);
+  this.connections[indexTo].splice(removeIndex2, 1);
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var i = 0; i < this.nodes.length; i++) {
+    cb(this.nodes[i]);
+  };
 };
-
+ 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
